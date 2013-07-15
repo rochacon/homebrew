1358e7b23e4695f692a120c5e364dbd85fe5792c
Dave Cheney <dave@cheney.net>
2013-07-15 18:06:07 +1000

    Fixups

--- Library/Formula/juju-core.rb
+++ Library/Formula/juju-core.rb
@@ -1,28 +1,26 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+VERSION='1.11.2'
 
 class JujuCore < Formula
   homepage 'https://launchpad.net/juju-core'
-  url 'https://launchpad.net/juju-core/trunk/1.11.2/+download/juju-core_1.11.2.tar.gz'
+  url "https://launchpad.net/juju-core/trunk/#{VERSION}/+download/juju-core_#{VERSION}.tar.gz"
   sha1 'd9579c3449711f24d3cd2ce251f1154bbfe490b9'
 
   depends_on 'go' => :build
 
   def install
     # set GOPATH to current working directory 
-    ENV['GOPATH']=Dir.pwd # wtf
+    ENV['GOPATH']=Dir.pwd
 
-    # fixup the 1.11.2 tarball 
+    # fixup the tarball, this is fixed in 1.11.3
     system "mkdir", "-p", "src"
     system "mv", "code.google.com", "src"
     system "mv", "labix.org", "src"
     system "mv", "launchpad.net", "src"
     system "mv", "github.com", "src"
-
     
-    system "go", "install", "-v", "launchpad.net/juju-core/cmd/juju"
+    system "go", "install", "launchpad.net/juju-core/cmd/juju"
     prefix.install(Dir['bin'])
   end
 
